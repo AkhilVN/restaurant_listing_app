a196de960fe27a24534796072ce52e45e7f5d00b
Akhil V N <53122437+AkhilVN@users.noreply.github.com>
2022-06-17 12:51:01 +0530

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 export const ENDPOINTS = {
-    BASEURL: `http://localhost:3001`,//`https://api.yelp.com/v3/businesses`,
+    BASEURL: `https://6f0f-49-207-222-228.ngrok.io`,//`https://api.yelp.com/v3/businesses`,
     SEARCH: `businesses/search`,
     DETAIL: `businesses`
 }
